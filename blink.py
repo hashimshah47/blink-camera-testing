4ff3cc550eab4a37f26faf549c5aa7b50c596c21
Hashim Tayyab Shah <hashimtayyab47@gmail.com>
2025-04-17 13:00:33 -0400

    keys updated

--- blink.py
+++ blink.py
@@ -20,52 +20,7 @@ headers = {
 data = '{ "password" : "' + sys.argv[2] + '", "email" : "' + sys.argv[1] + '" }'
 print(data)
 # res = requests.post('https://rest-u025.immedia-semi.com/api/v5/account/login', headers=headers, data=data)
-res = {
-    "account": {
-        "account_id": 468612,
-        "country": "CA",
-        "user_id": 468603,
-        "client_id": 1373420,
-        "client_trusted": False,
-        "new_account": False,
-        "tier": "u025",
-        "region": "ap",
-        "account_verification_required": False,
-        "phone_verification_required": False,
-        "client_verification_required": True,
-        "require_trust_client_device": True,
-        "country_required": False,
-        "verification_channel": "phone",
-        "user": {
-            "user_id": 468603,
-            "country": "CA"
-        },
-        "amazon_account_linked": False,
-        "braze_external_id": "3610e50ae58f767d5951e54a28fb03a3853844a2836324c7a91c1e3cf6c37d6a"
-    },
-    "auth": {
-        "token": "lTmrMTuBCoQkrLvWrD-YEA"
-    },
-    "phone": {
-        "number": "+1******6007",
-        "last_4_digits": "6007",
-        "country_calling_code": "1",
-        "valid": True
-    },
-    "verification": {
-        "email": {
-            "required": False
-        },
-        "phone": {
-            "required": True,
-            "channel": "sms"
-        }
-    },
-    "lockout_time_remaining": 0,
-    "force_password_reset": False,
-    "allow_pin_resend_seconds": 90
 
-}
 # print(json.dumps(res.json(), indent=4))  # 👈 Add this here
 authToken = "0yHSHACwAAS4F_zwiBlDPg"
 # res.json()["auth"]["token"]
